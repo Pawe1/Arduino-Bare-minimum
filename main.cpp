2b338bc62c3151d02dce0f5a436d38efda1b3109
PC <>
2017-01-23 13:53:39 +0100

    AVR version

--- main.cpp
+++ main.cpp
@@ -1,27 +1,14 @@
-#include <Arduino.h>
-
-// Declared weak in Arduino.h to allow user redefinitions.
-int atexit(void (* /*func*/ )()) { return 0; }
-
-// Weak empty variant initialization function.
-// May be redefined by variant files.
-void initVariant() __attribute__((weak));
-void initVariant() { }
-
-void setupUSB() __attribute__((weak));
-void setupUSB() { }
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
 int main(void)
 {
-	init();   // Initialize Arduino functionality. Requires Arduino.h
-
-	initVariant();
-
-#if defined(USBCON)
-	USBDevice.attach();   // Attach USB for applicable processors.
-#endif
-	
-	// ...
+	while (1)
+	{
+		// ...
+	}
 	
 	return 0;   // Execution should never reach this point.
 }
